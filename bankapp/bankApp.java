c65717d6cf5e3c8c6a0b08ee552b70d432b06884
Atiksh <atikshvaish@gmail.com>
2023-01-04 21:47:00 -0500

    gui

--- bankapp/bankApp.java
+++ bankapp/bankApp.java
@@ -5,38 +5,58 @@ public class bankApp {
         new swingGUI(); 
 
         bankAccount account; 
-
         account = new bankAccount(); 
 
+        bankAccount aliceAccount; 
+        aliceAccount = new bankAccount(); 
+        bankAccount bobAccount = aliceAccount; 
 
-        System.out.println("Input amount to deposit: ");
-        Scanner sc = new Scanner(System.in); 
-        int amount = sc.nextInt();         
-        account.deposit(amount); 
-        System.out.println("Balance is: " + account.getBalance());
+        
+        aliceAccount.deposit(1000);
+        System.out.println(aliceAccount.getBalance() + " " +bobAccount.getBalance());
 
-        System.out.println("Input amount to withdraw: ");
-        int depAmount = sc.nextInt(); 
+        bobAccount.withdraw(500);
+        System.out.println(aliceAccount.getBalance() + " " +bobAccount.getBalance());
+
+        bankAccount chuckAccount; 
+        chuckAccount = new bankAccount(500); 
+        System.out.println(chuckAccount.getBalance());
 
 
-        account.withdraw(depAmount); 
+
+        // System.out.println("Input amount to deposit: ");
+        // Scanner sc = new Scanner(System.in); 
+        // int amount = sc.nextInt();         
+        // account.deposit(amount); 
+        // System.out.println("Balance is: " + account.getBalance());
+
+        // System.out.println("Input amount to withdraw: ");
+        // int depAmount = sc.nextInt(); 
 
 
-        System.out.println("Balance is: " + account.getBalance());
+        // account.withdraw(depAmount); 
 
-        sc.close();
 
-        int a = 150000; 
-        int b = a; 
-        a += 300000; 
-        int c = b;
-        bankAccount x = new bankAccount(); 
-        x.deposit(150000);
-        bankAccount y = x; 
-        x.deposit(300000);
-        long z = y.getBalance();
-        System.out.println(c);
-        System.out.println(z);
+        // System.out.println("Balance is: " + account.getBalance());
+
+        // sc.close();
+
+        // int a = 150000; 
+        // int b = a; 
+        // a += 300000; 
+        // int c = b;
+        // bankAccount x = new bankAccount(); 
+        // x.deposit(150000);
+        // bankAccount y = x; 
+        // x.deposit(300000);
+        // long z = y.getBalance();
+        // System.out.println(c);
+        // System.out.println(z);
+
+
+
+
+
 
 
     }
--- bankapp/swingGUI.java
+++ bankapp/swingGUI.java
@@ -1,11 +1,61 @@
 package bankapp;
 import javax.swing.*;
+import javax.swing.LookAndFeel;
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+
+
+
 
 
 public class swingGUI {
     JFrame f; 
+
     swingGUI() {
+        f = new JFrame(); 
+
+        f.setTitle("JBA Online Banking");
+
+        JLabel l = new JLabel("Welconme to JBA Online Banking");
+        l.setBounds(50, 50, 300, 40); 
+        f.add(l); 
+
+        JButton n = new JButton("New Account"); 
+        n.setBounds(50, 100, 120, 40);
+        JButton d = new JButton("Deposit"); 
+        d.setBounds(50, 150, 120, 40);
+        JButton w = new JButton("Withdraw"); 
+        w.setBounds(180, 100, 120, 40);
+        JButton c = new JButton("Check Balance");
+        c.setBounds(180, 150, 120, 40);
+
+        f.add(n);
+        f.add(d);
+        f.add(w);
+        f.add(c);
+
+        JTextField t1 = new JTextField("Enter amount"); 
+        t1.setBounds(130, 300, 100, 40);
+        JButton b = new JButton("Submit"); 
+        b.setBounds(130, 350, 100, 40);
         
+        f.add(t1); 
+        f.add(b);
+
+        f.setSize(400, 500);
+        f.setLayout(null);
+        f.setVisible(true);
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize theme. Using fallback." );
+        }
+
+        
+
+
     }
     
 }
