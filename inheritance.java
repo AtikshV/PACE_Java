3e0be0f82bcab39c0174e392e63ddaadbb71d63c
Atiksh <atikshvaish@gmail.com>
2023-01-12 18:40:37 -0500

    labs

--- inheritance.java
+++ inheritance.java
@@ -1,28 +1,38 @@
+
+
 class Person {
     public String name; 
-    protected int age; 
+    private int age; 
 
-    public void setName(String n ) {
-        name = n;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int a) {
         age = a; 
     }
+
+    public int getAge() {
+        return this.age;
+    }
+    
+
 }
 
-class Student extends Person {
+class Student12 extends Person {
     public void display() {
         System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
+        System.out.println("Age: " + getAge());
     }
+
 }
 
 
 public class inheritance {
     public static void main(String[] args) {
-        Student s = new Student(); 
+        Student12 s = new Student12(); 
+        s.setName("hello");
+        s.setAge(10);
         s.display();
-
     }
 }
