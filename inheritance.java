667a2a0551c1b629853eeb3297f4355610bd3ae3
Atiksh <atikshvaish@gmail.com>
2023-01-11 21:38:57 -0500

    inheritance

--- inheritance.java
+++ inheritance.java
@@ -2,23 +2,52 @@ class Person {
     public String name; 
     int age; 
 
-    public void setName(String n ) {
-        name = n;
+    // public void setName(String n ) {
+    //     name = n;
+    // }
+
+    // public void setAge(int a) {
+    //     age = a; 
+    // }
+
+    public Person () {
+
     }
 
-     void setAge(int a) {
-        age = a; 
+    public Person(String n) {
+        this.name = n;
     }
 
+    public Person(int a) {
+        this.age = a; 
+    }
+
+    public void show() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Age: " + this.age);
+        // System.out.println("Birthday: " + bday); 
+
+    }
+
+
+}
+
+class Student {
+    String name; 
+    int age; 
+    int IDnum; 
+
+    public void setIDnum(int i) {
+     IDnum = i;    
+    }
 }
 
 public class inheritance {
     public static void main(String[] args) {
-        Person P = new Person(); 
-        P.setName("Niknok"); 
-        P.setAge(0);
-        System.out.println("Name: " + P.name);
-        System.out.println("Age: " + P.age);
+        Person P = new Person();
+        P.show(); 
+        // System.out.println("Name: " + P.name);
+        // System.out.println("Age: " + P.age);
         // System.out.println("Birthday: " + bday); 
 
         
